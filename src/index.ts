f855cb8840682547bad6be3e7d9b854e2cd6c420
tim.rohrer <tim@tgr66.dev>
2022-03-02 11:50:26 -0700

    feat: Convert project to library

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,3 @@
-import * as path from "path"
-import * as dotenv from "dotenv"
-import app from "./App"
+import { QuickenDataExtractor } from "./QuickenDataExtractor"
 
-const dotEnvPath = path.resolve("./.env")
-dotenv.config({ path: dotEnvPath })
-
-try {
-  const port = process.env.PORT || 3001
-  app
-    .listen(port, () => console.log(`server is listening on ${port}`))
-    .on("error", (err) => console.log(err))
-} catch (error) {
-  throw new Error("Error starting server.")
-}
+export default QuickenDataExtractor
